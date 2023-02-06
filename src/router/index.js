6bc6a6af9cdfddba880a0dff233d16cb7bfef366
chen.home <1147347984@qq.com>
2023-02-06 22:26:23 +0800

    feat: add router 404path

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,8 @@ import { createRouter, createWebHistory } from 'vue-router';
 const routes = [
 
   { path: '/', component: () => import('@/view/home/index.vue') },
-  { path: '/about', component: () => import('@/view/about/index.vue') }
+  { path: '/about', component: () => import('@/view/about/index.vue') },
+  { path: '/:pathMatch(.*)*', component: () => import('@/view/notFound/index.vue') }
 ]
 
 
---
+++ src/view/notFound/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="flex-center h-full">
+    404
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+
+</style>
